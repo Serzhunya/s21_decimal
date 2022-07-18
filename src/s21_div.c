42030b533541a6c8210aab633d7416f61d662c8e
Sandslash Dorla <sandslas@mi-k4.msk.21-school.ru>
2022-07-18 21:11:50 +0300

    dev

--- src/s21_div.c
+++ src/s21_div.c
@@ -690,57 +690,57 @@ int s21_zero(s21_decimal value) {
   return error;
 }
 
-int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-  // int exp1;
-  // int exp2;
-  // exp1 = s21_10_conv(value_1);
-  // exp2 = s21_10_conv(value_2);
-  // if (exp1 > exp2) {
-  //   value_2 = s21_superior_10(exp1, &value_2);
-  //   s21_rev_10_conv(result, exp1);
-  // } else if (exp2 > exp1) {
-  //   value_1 = s21_superior_10(exp2, &value_1);
-  //   s21_rev_10_conv(result, exp2);
-  // }
+// int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+//   // int exp1;
+//   // int exp2;
+//   // exp1 = s21_10_conv(value_1);
+//   // exp2 = s21_10_conv(value_2);
+//   // if (exp1 > exp2) {
+//   //   value_2 = s21_superior_10(exp1, &value_2);
+//   //   s21_rev_10_conv(result, exp1);
+//   // } else if (exp2 > exp1) {
+//   //   value_1 = s21_superior_10(exp2, &value_1);
+//   //   s21_rev_10_conv(result, exp2);
+//   // }
 
-  int erCode = 0;  // код ошибки
-                   /*  0 - OK
-                       1 - число слишком велико или равно положительной
-                       бесконечности 2 - число слишком мало или равно
-                       отрицательной бесконечности 3 - деление на ноль  */
-  /* 1) узнаем 31 бит bits[3] обоих чисел и запоминаем */
-  int sign_op_1 = 0;
-  int sign_op_2 = 0;
-  sign_op_1 = test_bit(value_1.bits[3], 31);
-  sign_op_2 = test_bit(value_2.bits[3], 31);
+//   int erCode = 0;  // код ошибки
+//                    /*  0 - OK
+//                        1 - число слишком велико или равно положительной
+//                        бесконечности 2 - число слишком мало или равно
+//                        отрицательной бесконечности 3 - деление на ноль  */
+//   /* 1) узнаем 31 бит bits[3] обоих чисел и запоминаем */
+//   int sign_op_1 = 0;
+//   int sign_op_2 = 0;
+//   sign_op_1 = test_bit(value_1.bits[3], 31);
+//   sign_op_2 = test_bit(value_2.bits[3], 31);
 
-  if (sign_op_1 == sign_op_2) {  // если знаки равны
-    erCode = s21_addiction_logic(value_1, value_2, result);
-    if (sign_op_1) {  // если отрицательный знак
-      set_1_bit(&result->bits[3], 31);
-    }
-  } else {
-    set_0_bit(&value_1.bits[3], 31);
-    set_0_bit(&value_2.bits[3], 31);
-    if (s21_is_greater(value_1, value_2) == 1) {
-      // то инвертируем value_1 и складываем с value_2
-      s21_invert_mantisa(&value_1);
-      s21_addiction_logic(value_1, value_2, result);
-      if (sign_op_1) {
-        set_1_bit(&result->bits[3], 31);
-      }
-    } else {
-      // то инвертируем value_2 и складываем с value_1
-      s21_invert_mantisa(&value_2);
-      s21_addiction_logic(value_1, value_2, result);
-      if (sign_op_2) {
-        set_1_bit(&result->bits[3], 31);
-      }
-    }
-    s21_invert_mantisa(result);
-  }
-  return (erCode);
-}
+//   if (sign_op_1 == sign_op_2) {  // если знаки равны
+//     erCode = s21_addiction_logic(value_1, value_2, result);
+//     if (sign_op_1) {  // если отрицательный знак
+//       set_1_bit(&result->bits[3], 31);
+//     }
+//   } else {
+//     set_0_bit(&value_1.bits[3], 31);
+//     set_0_bit(&value_2.bits[3], 31);
+//     if (s21_is_greater(value_1, value_2) == 1) {
+//       // то инвертируем value_1 и складываем с value_2
+//       s21_invert_mantisa(&value_1);
+//       s21_addiction_logic(value_1, value_2, result);
+//       if (sign_op_1) {
+//         set_1_bit(&result->bits[3], 31);
+//       }
+//     } else {
+//       // то инвертируем value_2 и складываем с value_1
+//       s21_invert_mantisa(&value_2);
+//       s21_addiction_logic(value_1, value_2, result);
+//       if (sign_op_2) {
+//         set_1_bit(&result->bits[3], 31);
+//       }
+//     }
+//     s21_invert_mantisa(result);
+//   }
+//   return (erCode);
+// }
 
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   int erCode = 0;
@@ -959,7 +959,7 @@ int division_in_10(s21_decimal value_1, s21_decimal *result) {
   s21_decimal one;
   s21_decimal res;
 
-  int count = 0;
+  int count = -1;
 
   ten.bits[0] = 0b00000000000000000000000000001010;  // 10
   ten.bits[1] = 0b00000000000000000000000000000000;
@@ -981,15 +981,17 @@ int division_in_10(s21_decimal value_1, s21_decimal *result) {
   set_0_bit(&value_1.bits[3], 31);
   int i = 1;
   // s21_is_greater_or_equal(value_1, ten) == 0
-  while (s21_is_equal(value_1, one) == 0 ||
-         s21_is_greater_or_equal(value_1, ten) == 1) {
+  while (s21_is_greater(value_1, ten)) {
     *result = s21_sub_in_10(value_1, result);
     value_1 = *result;
-    memset(result, 0, sizeof(*result));
     count++;
+    print_2(&value_1);
+    memset(result, 0, sizeof(*result));
+    printf("\n");
     // if (sign1 == 1) {
     //   set_1_bit(&result->bits[3], 31);
   }
+  // s21_invert_mantisa(result);
   return count;
 }
 
@@ -1007,7 +1009,26 @@ s21_decimal s21_sub_in_10(s21_decimal value_1, s21_decimal *result) {
   ten.bits[3] = 0b00000000000000000000000000000000;
 
   int error = 0;
-  s21_invert_mantisa(&value_1);
+
+  if (s21_is_greater_or_equal(value_1, ten) == 1) {
+    // то инвертируем value_1 и складываем с value_2
+    s21_invert_mantisa(&value_1);
+    // s21_addiction_logic(value_1, ten, result);
+    // if (sign_op_1) {
+    //   set_1_bit(&result->bits[3], 31);
+    // }
+  } else {
+    // то инвертируем value_2 и складываем с value_1
+    s21_invert_mantisa(&ten);
+    set_1_bit(&(result->bits[3]), 31);
+    // set_0_bit(&ten.bits[3], 31);
+    // s21_addiction_logic(value_1, ten, result);
+    // if (sign_op_2) {
+    //   set_1_bit(&result->bits[3], 31);
+    // }
+  }
+
+  // s21_invert_mantisa(&value_1);
   for (int j = 0; j < 3; j++) {
     for (int i = 0; i < 32; i++) {
       a = test_bit(value_1.bits[j], i);
@@ -1057,7 +1078,7 @@ int truncate(s21_decimal value, s21_decimal *result) {
   s21_decimal ten;
   // s21_decimal one;
 
-  ten.bits[0] = 0b00000000000000000000000001100100;  // 10
+  ten.bits[0] = 0b00000000000000000000000000011110;  // 10
   ten.bits[1] = 0b00000000000000000000000000000000;
   ten.bits[2] = 0b00000000000000000000000000000000;
   ten.bits[3] = 0b00000000000000000000000000000000;
@@ -1078,7 +1099,7 @@ int main(void) {
   s21_decimal dec1;
   s21_decimal result;
   s21_decimal ten;
-  dec1.bits[0] = 0b00000000000000000000000000011110;  // 23
+  dec1.bits[0] = 0b00000000000000000000000001000011;  // 23
   dec1.bits[1] = 0b00000000000000000000000000000000;
   dec1.bits[2] = 0b00000000000000000000000000000000;
   dec1.bits[3] = 0b00000000000000000000000000000000;
