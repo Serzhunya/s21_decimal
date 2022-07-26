dd4bd1d1942dc5a7959f02003052201cefaa8aa3
Tendyris Ragnor <tendyrir@oa-c5.msk.21-school.ru>
2022-07-26 12:57:10 +0300

    test commit

--- src/funcs/s21_truncate.c
+++ src/funcs/s21_truncate.c
@@ -1,60 +1,83 @@
 #include "s21_dec_lib.h"
 
+#define S21_MAX_UINT 4294967295
+
+
 int s21_truncate(s21_decimal value, s21_decimal *result) {
-  // s21_decimal res_value;
-  // s21_decimal result;
-  memset(result, 0, sizeof(*result));  // зануляем result
-  int exp = s21_10_conv(value);        // получаем степень
-  s21_decimal copy = value;            // делаем копию
-  s21_decimal ten;
-
-  ten.bits[0] = 0b00000000000000000000000000001010;  // 10
-  ten.bits[1] = 0b00000000000000000000000000000000;
-  ten.bits[2] = 0b00000000000000000000000000000000;
-  ten.bits[3] = 0b10000000000000000000000000000000;
-
-  // printf("\n value: \n");
-  // print_2(&value);
-
-  // printf("\n copy1: \n");
-  // print_2(&copy);
-
-  for (int i = 0; i < 32; i++) {      // убираем в ней степень
-    set_0_bit(&copy.bits[3], i);
+  int err_code = 0;
+  float res = 0.0;                                   // объявление кода ошибки
+  int scale = get_scale(&value);                     // получаем степень
+  if (scale) {                                       // пока степень положительная
+    s21_decimal tmp = {0};                           // зануляем темп децимал
+    s21_copy(value, &tmp);                           // копируем валуе по адресу темпа
+    s21_scale_decrease(&tmp, scale);                 // 
+    s21_copy(tmp, result);
+  } else {
+    s21_copy(value, result);
   }
-
-  // printf("\n copy2: \n");
-  // print_2(&copy);
-
-  // int sign = get_sign(&value);        // получаем знак
-
-  set_0_bit(&value.bits[3], 31);      // обнуляем знак
-
-  while (exp--) {
-    memset(result, 0, sizeof(*result));
-    
-    
-    // тут надо реализовать деление на двоичную десятку
-    // res_value - decimal
-    // надо в него записать результат деления на десять
-
-
-    // value - это то что надо поделить
-
-    // ten - это двоичная десятка 
-
-    // 
-
-    s21_div(value, ten, &result);
-
-    // print_2(&result);
-
-    // res_value = division_in_10(copy, result);    // записываем в результат на каждой итерации
-
-    // copy = res_value;                            // присваиваем копии результат
-    // print_2(&copy);
-  }
-
-  //*result = res_value;                           // кладем в резал
-  return 0;
+  // printf("это результат из функции:\n");
+  // print_2(result);
+  
+  // s21_from_decimal_to_float(*result, &res);
+  // printf("\n");
+  // printf("печатаем результат из функции = %f\n", res);
+  return err_code;
 }
+
+int get_scale(s21_decimal *d) { return (char)(d->bits[3] >> 16); }    // получаем интовое значение экспоненты
+
+void s21_copy(s21_decimal from, s21_decimal *to) {                    // копируем децималы
+  to->bits[0] = from.bits[0];
+  to->bits[1] = from.bits[1];
+  to->bits[2] = from.bits[2];
+  to->bits[3] = from.bits[3];
+}
+
+
+// первым параметром - указатель на децимал, вторым параметров интовый шифт ака скейл из функции
+
+void s21_scale_decrease(s21_decimal *value, int shift) {              // уменьшение степени
+  int i, tmp, j = 0;                      // три переменные i j - счетчики и tmp - буфер
+  unsigned long long overflow;            // беззнаковый лонг лонг для переполнения
+
+  printf("shift = %d\n", shift);
+
+  while (j < shift) {                     // пока счетчик равный 0 меньше значения степени (и увеличивается)
+
+    overflow = value->bits[2];            // в переполнение присваиваем значение из 3 инта децимался валуе
+    // printf("overflow1 = %llu\n", overflow);
+    i = 2;                                // и присваиваем второму счетчику i = 2
+  
+    while (i > 0) {                       // пока i больше нуля (и умеьншается) 
+
+      tmp = overflow % 10;                // присваиваем буферу остаток от деления значения переполения на 10
+      // printf("tmp1 = %d\n", tmp);
+
+      value->bits[i] = overflow / 10;
+      // printf("value->bits[%d] = %d\n", i, value->bits[i]);     // инт из структуры децимала равный по номеру i присваиваем переполнение деленное на 10 без остатка
+
+
+      i--;                                                  
+      overflow = tmp * (S21_MAX_UINT + 1) + value->bits[i]; // в переполнение присваивается буфер умноженный на (макс юинт + 1) + итый инт децимала
+      // printf("overflow2 = %llu\n", overflow);
+      //printf("value->bits[0] = %d\n", value->bits[0]);
+
+      if (i == 0) value->bits[i] = overflow / 10; // если и станет 0  в итый инт децимала присваиваем переполнение деленное на 10
+    } 
+    j++;
+  }
+
+//1111111111111111111111111111111
+//10000000000000000000000000000000
+  s21_set_scale(value, (s21_get_scale(*value) - shift)); // выставляем степень через получение интового значения разницы валуе и шифта
+
+}
+
+void s21_set_scale(s21_decimal *value, int size) {       // выставляем степень
+  value->bits[3] &= ~(S21_MAX_UINT / 2);                 // 
+  value->bits[3] >>= 16;
+  value->bits[3] |= size;
+  value->bits[3] <<= 16;
+}
+
+int s21_get_scale(s21_decimal value) { return (255 & (value.bits[3] >> 16)); }
--- src/tests/suite_truncate.c
+++ src/tests/suite_truncate.c
@@ -2,26 +2,39 @@
 
 START_TEST(s21_trun_1) {
   s21_decimal dec1;
-  dec1.bits[0] = 0b00000000000000000000000011010011; // 21.1
-  dec1.bits[1] = 0b00000000000000000000000000000000;
+                //31
+  dec1.bits[0] = 0b10000000000000000000000000010001; 
+  dec1.bits[1] = 0b00000000000000000000000000000000; // 214748366.5
   dec1.bits[2] = 0b00000000000000000000000000000000;
-  dec1.bits[3] = 0b00000000000000010000000000000000;
-  float num = 0.0;
-  s21_from_decimal_to_float(dec1, &num);
-  printf("%f", num);
-  s21_decimal result;
-  result.bits[0] = 0b00000000000000000000000000010101;
-  result.bits[1] = 0b00000000000000000000000000000000;
-  result.bits[2] = 0b00000000000000000000000000000000;
-  result.bits[3] = 0b00000000000000000000000000000000;
+  dec1.bits[3] = 0b00000000000001110000000000000000;
+                //        |        <   <-9876543210  
+  // float num = 0.0;
+  // s21_from_decimal_to_float(dec1, &num);
+  // printf("\n");
+  // printf("печатаем цель из теста = %f\n", num);
+  // s21_decimal result;
+  // result.bits[0] = 0b10000011000100100110111010010111; // 18 446 744 073 709 551 , 617 
+  // result.bits[1] = 0b10001101010011111101111000000000; 
+  // result.bits[2] = 0b00000000000000000000000000000000;
+  // result.bits[3] = 0b00000000000000000000000000000000;
   s21_decimal res1;
   s21_truncate(dec1, &res1);
-  ck_assert_float_eq(res1.bits[0], result.bits[0]);
-  ck_assert_float_eq(res1.bits[1], result.bits[1]);
-  ck_assert_float_eq(res1.bits[2], result.bits[2]);
-  ck_assert_float_eq(res1.bits[3], result.bits[3]);
+  // ck_assert_int_eq(res1.bits[0], result.bits[0]);
+  // ck_assert_int_eq(res1.bits[1], result.bits[1]);
+  // ck_assert_int_eq(res1.bits[2], result.bits[2]);
+  // ck_assert_int_eq(res1.bits[3], result.bits[3]);
+
+  // print_2(&dec1);
+  printf("\n");
+  printf("res1\n");// 214748366.5
+  print_2(&res1);  // 2362232014
+
 }
 END_TEST
+
+
+
+
 
 // START_TEST(s21_trun_2) {
 //   s21_decimal dec1; //
@@ -32,9 +45,9 @@ END_TEST
 //   float num = 0.0;
 //   s21_from_decimal_to_float(dec1, &num);
 //   printf("\n");
-//   printf("%f", num);
+//   printf("%f\n", num);
 //   s21_decimal result;
-//   result.bits[0] = 0b00000000000000000000000000010001;
+//   result.bits[0] = 0b00000000000000000000000000010000;
 //   result.bits[1] = 0b00000000000000000000000000000000;
 //   result.bits[2] = 0b00000000000000000000000000000000;
 //   result.bits[3] = 0b00000000000000000000000000000000;
